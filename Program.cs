e365c0722c27c79f9ff671599ae0fbd3355ee52c
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-21 13:57:38 +0800

    Инициализация переменных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+Console.WriteLine("Введите размер массива");
+int n = int.Parse(Console.ReadLine());
+
+string[] array = new string[n];
+
+Console.WriteLine("Введите элементы массива");
+
+for (int i = 0; i < n; ++i)
+{
+    Console.WriteLine("Элемент {0}:", i);
+    array[i] = Console.ReadLine();
+}
